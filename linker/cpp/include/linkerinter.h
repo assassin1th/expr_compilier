3338a410a024011c26ba0f9904647d7ddfb114ee
Shushuev_Artemiy <assassin1th@gmail.com>
2019-06-22 22:15:59 +0300

    add linker inter classes: sym, symlink, definedsymlink

--- linker/cpp/include/linkerinter.h
+++ linker/cpp/include/linkerinter.h
@@ -2,17 +2,22 @@
 #define LINKER_INTER_H_
 
 #include <string>
+#include "comp/cpp/include/lexer.h"
 
 namespace LinkerInter {
+
+enum SymTag
+{
+    DEFINED, UNDEFINED
+};
 
 class Code
 {
 public:
     Code();
     virtual ~Code();
-    virtual const std::string &gen() const;
-protected:
-    static std::string tmp_string;
+    virtual const std::string gen() const;
+    virtual const Code *reduce() const;
 };
 
 class ByteCode : public Code
@@ -20,9 +25,39 @@ class ByteCode : public Code
 public:
     ByteCode(const std::string &bytes);
     virtual ~ByteCode();
-    virtual const std::string &gen() const;
+    virtual const std::string gen() const;
 private:
     const std::string m_bytes;
+};
+
+class Sym : public Code
+{
+public:
+    Sym(const CompLexer::Token *tok);
+    virtual ~Sym();
+    virtual const std::string gen() const;
+    virtual const Code *reduce() const;
+protected:
+    const CompLexer::Token *m_tok;
+};
+
+class SymLink : public Sym
+{
+public:
+    SymLink(const CompLexer::Token *tok, SymTag tag = SymTag::UNDEFINED);
+    virtual ~SymLink();
+private:
+    const SymTag m_tag;
+};
+
+class DefinedSymLink : public SymLink
+{
+public:
+    DefinedSymLink(const CompLexer::Token *tok, int16_t offset);
+    virtual ~DefinedSymLink();
+    virtual const std::string gen() const;
+private:
+    const int16_t m_offset;
 };
 
 }
--- linker/cpp/src/linkerinter.cpp
+++ linker/cpp/src/linkerinter.cpp
@@ -2,8 +2,6 @@
 
 using namespace LinkerInter;
 
-static std::string tmp_string = "";
-
 Code::Code()
 {
 }
@@ -12,10 +10,16 @@ Code::~Code()
 {
 }
 
-const std::string &
+const std::string
 Code::gen() const
 {
-    return tmp_string = "";
+    return "";
+}
+
+const Code *
+Code::reduce() const
+{
+    return this;
 }
 
 ByteCode::ByteCode(const std::string &bytes) :
@@ -27,8 +31,55 @@ ByteCode::~ByteCode()
 {
 }
 
-const std::string &
+const std::string
 ByteCode::gen() const
 {
     return m_bytes;
 }
+
+Sym::Sym(const CompLexer::Token *tok) :
+    Code(), m_tok(tok)
+{
+}
+
+Sym::~Sym()
+{
+}
+
+const std::string
+Sym::gen() const
+{
+    return "";
+}
+
+const Code *
+Sym::reduce() const
+{
+    return this;
+}
+
+SymLink::SymLink(const CompLexer::Token *tok, SymTag tag) :
+    Sym(tok), m_tag(tag)
+{
+}
+
+SymLink::~SymLink()
+{
+}
+
+DefinedSymLink::DefinedSymLink(const CompLexer::Token *tok, int16_t offset) :
+    SymLink(tok, SymTag::DEFINED), m_offset(offset)
+{
+}
+
+DefinedSymLink::~DefinedSymLink()
+{
+}
+
+const std::string
+DefinedSymLink::gen() const
+{
+    return std::string((char *) &m_offset, sizeof (int16_t));
+}
+
+
